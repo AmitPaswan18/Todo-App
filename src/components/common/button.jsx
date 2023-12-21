c80b8784be077b7029b4fe84dce9d030d119abb3
Amit Paswan <ak9425391@gmail.com>
2023-12-21 11:03:37 +0530

    local storage added

---
+++ src/components/common/button.jsx
@@ -0,0 +1,11 @@
+const button = () => {
+  return (
+    <div className="flex ">
+      <button className="bg-blue-500 p-2 rounded-md" type="submit">
+        Add a Task
+      </button>
+    </div>
+  );
+};
+
+export default button;
