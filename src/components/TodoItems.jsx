fda984ce65399dd45bd024fc79acbe3e90b8bd15
Amit Paswan <ak9425391@gmail.com>
2023-12-25 23:45:40 +0530

    click outside save edit todo bug fixed

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -107,171 +107,197 @@ const TodoItems = () => {
     localStorage.setItem("todo", JSON.stringify(todo));
   }
 
+  const containerRef = useRef(null);
+
+  const handleBlur = (event) => {
+    if (editIndex !== null) {
+      handleEditSubmit(event);
+    }
+  };
+
   useEffect(() => {
     loadTasks();
-    setInputValid(true);
+    const handleKeyDown = (event) => {
+      if (event.key === "Enter") {
+        if (editIndex !== null) {
+          handleEditSubmit(event);
+        }
+      }
+    };
+
     const handleClickOutside = (event) => {
-      if (editRef.current && !editRef.current.contains(event.target)) {
+      if (
+        containerRef.current &&
+        !containerRef.current.contains(event.target)
+      ) {
+        if (editIndex !== null) {
+          handleEditSubmit(event);
+        }
         cancelEdit();
       }
     };
 
-    document.addEventListener("click", handleClickOutside);
+    document.addEventListener("keydown", handleKeyDown);
+    document.addEventListener("mousedown", handleClickOutside);
 
     return () => {
-      document.removeEventListener("click", handleClickOutside);
+      document.removeEventListener("keydown", handleKeyDown);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
-  }, []);
+  }, [containerRef, editIndex]);
 
   return (
     <>
-      <div
-        id="scrollbar"
-        className="flex justify-center  lg:w-1/2 w-10/12  mt-9 flex-col items-center mx-auto border-2 rounded-md p-2 bg-gray-700">
-        <div className=" max-h-96 overflow-y-auto w-11/12 flex justify-center lg:w-10/12  ">
-          {todo.length === 0 ? (
-            <>
-              <div className=" flex flex-col md:flex-col justify-evenly md:w-full md:z-10 lg:w-1/3 text-white lg:ml-10  lg:h-3/6 sm:h-1/2 ">
-                <div className="font-sans lg:font-extrabold  lg:text-3xl text-sm font-bold my-2">
+      <div className="w-full" ref={containerRef}>
+        <div
+          id="scrollbar"
+          className="flex justify-center  lg:w-1/2 w-10/12  mt-9 flex-col items-center mx-auto border-2 rounded-md p-2 bg-gray-700">
+          <div className=" max-h-96 overflow-y-auto w-11/12 flex justify-center lg:w-10/12  ">
+            {todo.length === 0 ? (
+              <>
+                <div className=" flex flex-col md:flex-col justify-evenly md:w-full md:z-10 lg:w-1/3 text-white lg:ml-10  lg:h-3/6 sm:h-1/2 ">
+                  <div className="font-sans lg:font-extrabold  lg:text-3xl text-sm font-bold my-2">
+                    My Tasks
+                  </div>
+                  <div className=" text-md text-slate-500 opacity-40 py-2 ">
+                    {" "}
+                    You have No Task
+                  </div>
+                </div>
+              </>
+            ) : (
+              <div className=" left-10 lg:w-10/12 w-full max-h-fit">
+                <div className="font-sans text-4xl font-bold text-white">
                   My Tasks
                 </div>
-                <div className=" text-md text-slate-500 opacity-40 py-2 ">
-                  {" "}
-                  You have No Task
-                </div>
-              </div>
-            </>
-          ) : (
-            <div className=" left-10 lg:w-10/12 w-full max-h-fit">
-              <div className="font-sans text-4xl font-bold text-white">
-                My Tasks
-              </div>
 
-              {isinputValid === false && (
-                <div className="text-red-600">
-                  Please enter a valid task name
-                </div>
-              )}
-              {todo.map((item, index) => (
-                <div className=" max-h-screen text-white" key={index}>
-                  <div
-                    ref={editIndex === index ? editRef : null}
-                    className="flex max-h-fit max-w-fit my-2 bg-slates w-full  border-2 border-gray-500 rounded-md justify-between py-1 px-1">
-                    {editIndex === index ? (
-                      <>
-                        <div className="flex flex-col">
-                          <InputButton
-                            className="w-full h-8 rounded-md p-2 m-2 text-black"
-                            placeholder="Task..."
-                            onChange={(e) => setTitleEdit(e.target.value)}
-                            onKeyDown={(e) => {
-                              if (e.key === "Enter") {
-                                handleEditSubmit(e);
-                              }
-                            }}
-                          />
+                {isinputValid === false && (
+                  <div className="text-red-600">
+                    Please enter a valid task name
+                  </div>
+                )}
+                {todo.map((item, index) => (
+                  <div className=" max-h-screen text-white" key={index}>
+                    <div
+                      ref={editIndex === index ? editRef : null}
+                      className="flex max-h-fit max-w-fit my-2 bg-slates w-full  border-2 border-gray-500 rounded-md justify-between py-1 px-1">
+                      {editIndex === index ? (
+                        <>
+                          <div className="flex flex-col">
+                            <InputButton
+                              className="w-full h-8 rounded-md p-2 m-2 text-black"
+                              placeholder="Task..."
+                              onChange={(e) => setTitleEdit(e.target.value)}
+                              onBlur={handleBlur}
+                              onKeyDown={(e) => {
+                                if (e.key === "Enter") {
+                                  handleEditSubmit(e);
+                                }
+                              }}
+                            />
+                          </div>
+                        </>
+                      ) : (
+                        <div className="flex lg:mx-2 lg:gap-2 gap-0 flex-col">
+                          <div className="md:text-sm text-xs">{item.title}</div>
+                          <div>
+                            <Checkboxbutton
+                              checked={checkedItems[index] || false}
+                              onChange={() => handleCheckboxChange(index)}
+                            />
+                            {checkedItems[index] == true ? (
+                              <span className="badge text-bg-success m-2">
+                                Complete
+                              </span>
+                            ) : null}
+                          </div>
                         </div>
-                      </>
-                    ) : (
+                      )}
+                      <div className="flex flex-col md:flex-row">
+                        <RiDeleteBin5Fill
+                          className="m-1"
+                          color="red"
+                          opacity={0.6}
+                          size={18}
+                          style={{ cursor: "pointer" }}
+                          onClick={() => deleteTodo(index)}
+                        />
+                        <RiEditBoxLine
+                          className="m-1"
+                          color="white"
+                          opacity={0.6}
+                          size={18}
+                          style={{ cursor: "pointer" }}
+                          onClick={() =>
+                            editIndex === index
+                              ? handleEditSubmit()
+                              : editTodo(index)
+                          }
+                        />
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
+            <ToogleBar />
+          </div>
+          <div className=" max-h-fit flex  w-10/12 justify-center ">
+            <form
+              className="w-full max-w-fit lg:w-11/12"
+              name="todoform"
+              onSubmit={handleSubmit}>
+              <div className="text-white mr-8">
+                {" "}
+                <div className="flex flex-col w-full">
+                  <div>Task</div>
+                  <InputButton
+                    type="text"
+                    className="w-full h-8 rounded-md p-2 my-2 text-black "
+                    placeholder="Task..."
+                    onChange={(e) => setTitle(e.target.value)}
+                    value={title}
+                  />
+                </div>
+                <div className="flex ">
+                  <Button color="#1864AB"> Add a Task</Button>
+                </div>
+                <div className="flex justify-center mt-4 gap-2 text-lg">
+                  <RadioButton
+                    label="All"
+                    value="all"
+                    checked={filter === "all"}
+                    onChange={() => handleFilterChange("all")}
+                  />
+
+                  <RadioButton
+                    label="Complete"
+                    value="complete"
+                    checked={filter === "complete"}
+                    onChange={() => handleFilterChange("complete")}
+                  />
+
+                  <label htmlFor="incomplete">
+                    <RadioButton
+                      label="Incomplete"
+                      value="incomplete"
+                      checked={filter === "incomplete"}
+                      onChange={() => handleFilterChange("incomplete")}
+                    />{" "}
+                  </label>
+                </div>
+                {filteredTodo().map((item, index) => (
+                  <div className=" max-h-screen text-white" key={index}>
+                    <div className="flex max-h-fit max-w-fit my-2 bg-slates w-full  border-2 border-gray-500 rounded-md justify-between py-1 px-1">
                       <div className="flex lg:mx-2 lg:gap-2 gap-0 flex-col">
                         <div className="md:text-sm text-xs">{item.title}</div>
-                        <div>
-                          <Checkboxbutton
-                            checked={checkedItems[index] || false}
-                            onChange={() => handleCheckboxChange(index)}
-                          />
-                          {checkedItems[index] == true ? (
-                            <span className="badge text-bg-success m-2">
-                              Complete
-                            </span>
-                          ) : null}
-                        </div>
                       </div>
-                    )}
-                    <div className="flex flex-col md:flex-row">
-                      <RiDeleteBin5Fill
-                        className="m-1"
-                        color="red"
-                        opacity={0.6}
-                        size={18}
-                        style={{ cursor: "pointer" }}
-                        onClick={() => deleteTodo(index)}
-                      />
-                      <RiEditBoxLine
-                        className="m-1"
-                        color="white"
-                        opacity={0.6}
-                        size={18}
-                        style={{ cursor: "pointer" }}
-                        onClick={() =>
-                          editIndex === index
-                            ? handleEditSubmit()
-                            : editTodo(index)
-                        }
-                      />
                     </div>
                   </div>
-                </div>
-              ))}
-            </div>
-          )}
-          <ToogleBar />
-        </div>
-        <div className=" max-h-fit flex  w-10/12 justify-center ">
-          <form
-            className="w-full max-w-fit lg:w-11/12"
-            name="todoform"
-            onSubmit={handleSubmit}>
-            <div className="text-white mr-8">
-              {" "}
-              <div className="flex flex-col w-full">
-                <div>Task</div>
-                <InputButton
-                  type="text"
-                  className="w-full h-8 rounded-md p-2 my-2 text-black "
-                  placeholder="Task..."
-                  onChange={(e) => setTitle(e.target.value)}
-                  value={title}
-                />
+                ))}
               </div>
-              <div className="flex ">
-                <Button color="#1864AB"> Add a Task</Button>
-              </div>
-              <div className="flex justify-center mt-4 gap-2 text-lg">
-                <RadioButton
-                  label="All"
-                  value="all"
-                  checked={filter === "all"}
-                  onChange={() => handleFilterChange("all")}
-                />
-
-                <RadioButton
-                  label="Complete"
-                  value="complete"
-                  checked={filter === "complete"}
-                  onChange={() => handleFilterChange("complete")}
-                />
-
-                <label htmlFor="incomplete">
-                  <RadioButton
-                    label="Incomplete"
-                    value="incomplete"
-                    checked={filter === "incomplete"}
-                    onChange={() => handleFilterChange("incomplete")}
-                  />{" "}
-                </label>
-              </div>
-              {filteredTodo().map((item, index) => (
-                <div className=" max-h-screen text-white" key={index}>
-                  <div className="flex max-h-fit max-w-fit my-2 bg-slates w-full  border-2 border-gray-500 rounded-md justify-between py-1 px-1">
-                    <div className="flex lg:mx-2 lg:gap-2 gap-0 flex-col">
-                      <div className="md:text-sm text-xs">{item.title}</div>
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
       </div>
     </>
